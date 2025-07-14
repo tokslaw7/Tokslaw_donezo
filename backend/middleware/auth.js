d1181f47191b5994168a38a3b3820b2f7a95eb6c
tokslaw7 <147814066+tokslaw-shruboak@users.noreply.github.com>
2025-07-14 17:29:23 -0400

    backend

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,5 +1,5 @@
 
-//Put code here for the middleware to authenticate User requests if they have the token and check if it's a legit token
+//middleware to authenticate User requests if they have the token and check if it's a legit token
 
 // Import the `jsonwebtoken` module for verifying and creating JWTs
 import jwt from 'jsonwebtoken';
