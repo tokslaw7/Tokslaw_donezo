0d3ea606a9138c0dd1d82d61621dd1a6311ef865
tokslaw7 <taborg@MacBookPro.lan>
2025-07-21 21:50:05 -0400

    New Todo

--- frontend/src/axios-instance.js
+++ frontend/src/axios-instance.js
@@ -2,6 +2,16 @@ import axios from "axios";
 import supabase from "./client";
 
 const getAxiosClient = async () => {
+
+    const currentSession = await supabase.auth.getSession();
+
+    const instance = axios.create({
+        headers: {
+            Authorization: `Bearer ${currentSession.data.session.access_token}`,
+        },
+    });
+
+    return instance;
   // Get the supabase session
   // create an axios instance with the supabase access_token
   // Return the instance
--- frontend/src/pages/todos.jsx
+++ frontend/src/pages/todos.jsx
@@ -1,20 +1,13 @@
 import { useRef } from "react";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { useForm } from "react-hook-form";
+import getAxiosClient from "../axios-instance";
+
 
 export default function Todos() {
   const modalRef = useRef();
+  const queryClient = useQueryClient();
 
-  const toggleNewTodoModal = () => {
-    // Check if the modal is currently open by accessing the `open` property of `modalRef`.
-    if (modalRef.current.open) {
-      // If the modal is open, close it by calling the `close()` method.
-      modalRef.current.close();
-    } else {
-      // If the modal is not open, open it by calling the `showModal()` method.
-      modalRef.current.showModal();
-    }
-  };
 
   const {register, handleSubmit} = useForm({
     defaultValues: {
@@ -23,56 +16,170 @@ export default function Todos() {
     }
   });
 
+
+  const {data, isError, isLoading} = useQuery({
+    queryKey: ["todos"],
+    queryFn: async () => {
+      const axiosInstance = await getAxiosClient();
+
+      const {data} = await axiosInstance.get("http://localhost:8080/todos");
+      // const { data } = await axios.get("http://localhost:8080/todos")
+
+      return data;
+    }
+  });
+
+  const {mutate: createNewTodo} = useMutation({
+    mutationKey: ["newTodo"],
+    mutationFn: async (newTodo) => {
+      const axiosInstance = await getAxiosClient();
+
+      const {data} = await axiosInstance.post("http://localhost:8080/todos", newTodo);
+
+      return data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries("todos");
+    }
+  });
+
+  const {mutate: markAsCompleted} = useMutation({
+    mutationKey: ["markAsCompleted"],
+    mutationFn: async (todoId) => {
+      const axiosInstance = await getAxiosClient();
+
+      const {data} = await axiosInstance.put(`http://localhost:8080/todos/${todoId}/completed`);
+
+      return data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries("todos");
+    }
+  });
+
+  if (isLoading) {
+    return (
+        <div className="">Loading Todos...</div>
+    )
+  }
+
+  if (isError) {
+    return (
+        <div className="">There was an error</div>
+    )
+  }
+
+  console.log(data);
+
+  const toggleNewTodoModal = () => {
+    if (modalRef.current.open) {
+      modalRef.current.close();
+    } else {
+      modalRef.current.showModal();
+    }
+  }
+
   const handleNewTodo = (values) => {
+    createNewTodo(values)
     toggleNewTodoModal();
+  }
+
+  function NewTodoButton() {
+    return (
+        <button className="btn btn-primary" onClick={() => toggleNewTodoModal()}>
+          New Todo
+        </button>
+    )
+  }
+
+  function TodoModal() {
+    return (
+        <>
+          <dialog ref={modalRef} className="modal">
+            <dialog className="modal">
+              <div className="modal-box">
+                <h3 className="font-bold text-lg">New Todo</h3>
+                <form onSubmit={handleSubmit(handleNewTodo)}>
+                  <label className="form-control w-full">
+                    <div className="label">
+                      <span className="label-text">Name of Todo</span>
+                    </div>
+                    <input
+                        type="text"
+                        placeholder="Type here"
+                        className="input input-bordered w-full"
+                        {...register("name")} />
+                    />
+                  </label>
+                  <label className="form-control w-full">
+                    <div className="label">
+                      <span className="label-text">Description</span>
+                    </div>
+                    <input
+                        type="text"
+                        placeholder="Type here"
+                        className="input input-bordered w-full"
+                        {...register("description")} />
+                    />
+                  </label>
+                  <div className="modal-action">
+                    <button type="submit" className="btn btn-primary">
+                      Create Todo
+                    </button>
+                    <button type="button" className="btn btn-ghost" onClick={toggleNewTodoModal}>
+                      Close
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </dialog>
+          </dialog>
+        </>
+    );
+  }
+
+
+  function TodoItemList() {
+    return (
+        <div className="w-lg h-sm flex column items-center justify-center gap-4">
+          {data.success && data.todos.length >= 1 && (
+              <ul className="flex column items-center justify-center gap-4">
+                {
+                  data.todos.map(todo => (
+                      <li className="inline-flex items-center gap-4">
+                        <div className="w-md">
+                          <h3 className="text-lg">
+                            {todo.name}
+                          </h3>
+                          <p className="text-sm">{todo.description}</p>
+                        </div>
+                        <div className="w-md">
+                          <label className="swap">
+                            <input type="checkbox" onClick={() => markAsCompleted(todo.id)}/>
+                            <div className="swap-on">
+                              Yes
+                            </div>
+                            <div className="swap-off">
+                              No
+                            </div>
+                          </label>
+                        </div>
+                      </li>
+                  ))
+                }
+              </ul>
+          )}</div>
+    )
   }
 
   return (
       <>
-        <button className="btn btn-primary" onClick={() => toggleNewTodoModal()}>
-          New Todo
-        </button>
-
-        <dialog ref={modalRef} className="modal">
-          <dialog className="modal">
-            <div className="modal-box">
-              <h3 className="font-bold text-lg">New Todo</h3>
-              <form onSubmit={handleSubmit(handleNewTodo)}>
-                <label className="form-control w-full">
-                  <div className="label">
-                    <span className="label-text">Name of Todo</span>
-                  </div>
-                  <input
-                      type="text"
-                      placeholder="Type here"
-                      className="input input-bordered w-full"
-                      {...register("name")} />
-                  />
-                </label>
-                <label className="form-control w-full">
-                  <div className="label">
-                    <span className="label-text">Description</span>
-                  </div>
-                  <input
-                      type="text"
-                      placeholder="Type here"
-                      className="input input-bordered w-full"
-                      {...register("description")} />
-                  />
-                </label>
-                <div className="modal-action">
-                  <button type="submit" className="btn btn-primary">
-                    Create Todo
-                  </button>
-                  <button type="button" className="btn btn-ghost" onClick={toggleNewTodoModal}>
-                    Close
-                  </button>
-                </div>
-              </form>
-            </div>
-          </dialog>
-        </dialog>
+        <NewTodoButton/>
+        <TodoItemList/>
+        <TodoModal/>
       </>
-  );
+  )
 }
+
+
 
